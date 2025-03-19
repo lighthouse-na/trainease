aef4b70ad0f81e2331ee889c96059da630482c35
hpmouton <hmouton786@gmail.com>
2025-03-19 16:23:38 +0200

    wip

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -5,7 +5,7 @@ use App\Models\UserDetail;
 
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-test('guests are redirected to the login page', function () {
+test('Authenticated users are are redirected to the login page', function () {
     $response = $this->get('/dashboard');
     $response->assertRedirect('/login');
 });
